fa3a850721aa0121bbd05b3de335131fb8a0855b
avk <Andrew.Kostousov@gmail.com>
2018-05-07 18:39:35 +0500

    show tests stdout in appveyor; autodetect jdk8 home

--- src/SystemTimerTester/.build-and-run.cmd
+++ src/SystemTimerTester/.build-and-run.cmd
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_131
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Development Kit\1.8" /v JavaHome') DO set JAVA_HOME=%%B
 "%JAVA_HOME%\bin\javac.exe" -target 1.8 SystemTimerTester.java || exit /b 1
 "%JAVA_HOME%\bin\java.exe" -classpath .\ SystemTimerTester || exit /b 1
 pause
